918a43385e39794dbb608c31e71ad27ee1ce781d
immamadeshon <mani.darvish32@gmail.com>
2025-10-19 19:17:02 +0330

    themeable calc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 const btn = document.querySelector(".menu");
-const theme1 = document.querySelectorAll(".numb-color");
-const theme2 = document.querySelectorAll(".numb-color-1");
-const theme3 = document.querySelectorAll(".sign-color");
-const theme4 = document.querySelectorAll(".sign-color-1");
-const border1 = document.querySelector(".selector-border");
-const border2 = document.querySelector(".selector-border-1");
+// const theme1 = document.querySelectorAll(".numb-color");
+// const theme2 = document.querySelectorAll(".numb-color-1");
+// const theme3 = document.querySelectorAll(".sign-color");
+// const theme4 = document.querySelectorAll(".sign-color-1");
+// const border1 = document.querySelector(".selector-border");
+// const border2 = document.querySelector(".selector-border-1");
 
 btn.addEventListener("click", () => {
   const elements = document.querySelectorAll(".keys-numb");
@@ -24,7 +24,7 @@ btn.addEventListener("click", () => {
   elements.forEach(el => {
     el.classList.toggle('sign-color' , !istheme);
     el.classList.toggle('sign-color-1' , istheme);
-    el.classList.toggle('sign-color-2' , istheme1);
+    el.classList.toggle('sign-color-2', istheme1);
     el.classList.toggle('sign-color' , !istheme1);
   });
 });
@@ -39,3 +39,20 @@ btn.addEventListener("click", () => {
     el.classList.toggle('selector-border' , !istheme1);
   });
 });
+
+// btn.addEventListener("click", () => {
+//   const elements = document.querySelectorAll(".keys");
+//   const istheme = elements[0].classList.contains("sign-color-2");
+//   elements.forEach(el => {
+//     el.classList.toggle('keys-2', !istheme);
+//     el.classList.toggle('keys-2', istheme);
+//   });
+// });
+// btn.addEventListener("click", () => {
+//   const elements = document.querySelectorAll(".keys");
+//   const istheme = elements[0].classList.contains("numb-color-2");
+//   elements.forEach(el => {
+//     el.classList.toggle('keys-numb-2', !istheme);
+//     el.classList.toggle('keys-numb-2', istheme);
+//   });
+// });
